c355ff163a44fdc846f0ce913d1d03b00344f1bc
JERRYFROMKENYA <jerryochieng178@gmail.com>
2023-05-14 17:42:27 +0000

    Final Commit

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,25 @@
 import numpy as np
 
 def calculate(list):
+    np.set_printoptions(precision=16)
+
+    if len(list)==9:
+        x=np.array(list,dtype='float')
+        
+        nnp=x.reshape(3,3)
+                
+        calculations={
+                'mean': [nnp.mean(axis=0), nnp.mean(axis=1), nnp.mean()],
+                'variance': [nnp.var(axis=0), nnp.var(axis=1), nnp.var()],
+                'standard deviation': [nnp.std(axis=0), nnp.std(axis=1), nnp.std()],
+                'max': [nnp.max(axis=0), nnp.max(axis=1), nnp.max()],
+                'min': [nnp.min(axis=0), nnp.min(axis=1), nnp.min()],
+                'sum': [nnp.sum(axis=0), nnp.sum(axis=1), nnp.sum()]
+                    }
+        return calculations
+    else:
+        raise ValueError("List must contain nine numbers.")
 
 
 
 
-    return calculations
